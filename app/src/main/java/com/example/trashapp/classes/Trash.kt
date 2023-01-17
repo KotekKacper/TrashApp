acb0aaae60809091ba06fd302c2c85c39fc71de0
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-01-17 22:15:51 +0100

    Converted Trash class to data class

--- app/src/main/java/com/example/trashapp/classes/Trash.kt
+++ app/src/main/java/com/example/trashapp/classes/Trash.kt
@@ -3,29 +3,14 @@ package com.example.trashapp.classes
 import java.sql.Timestamp
 import java.time.Instant
 
-class Trash {
-    var id: Int = 0
-    var localization: String = ""
-    var creationDate: Timestamp? = null
-    var trashSize: Int = 0
-    var vehicleId: Int? = null
-    var userLoginReport: String? = null
-    var cleaningCrewId: Int? = null
-    var userLogin: String? = null
+data class Trash(
+    var id: Int,
+    var localization: String,
+    var creationDate: Timestamp?,
+    var trashSize: Int,
+    var vehicleId: Int?,
+    var userLoginReport: String?,
+    var cleaningCrewId: Int?,
+    var userLogin: String?,
     var collectionDate: Timestamp? = null
-
-    constructor(id: Int, localization: String, creationDate: Timestamp, trashSize: Int, vehicleId: Int?, userLoginReport: String?, cleaningCrewId:Int?, userLogin:String?, collectionDate: Timestamp?=null)
-    {
-        this.id = id
-        this.localization = localization
-        this.creationDate = creationDate
-        this.trashSize = trashSize
-        this.vehicleId = vehicleId
-        this.userLoginReport = userLoginReport
-        this.cleaningCrewId = cleaningCrewId
-        this.userLogin = userLogin
-        this.collectionDate = collectionDate
-    }
-    
-
-}
+)
