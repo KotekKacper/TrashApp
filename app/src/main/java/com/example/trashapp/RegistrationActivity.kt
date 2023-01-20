c1d5197ebd95cda0ae6ab6a5c176f8a89b69bd1d
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-01-20 14:40:36 +0100

    Improved registration screen

--- app/src/main/java/com/example/trashapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/trashapp/RegistrationActivity.kt
@@ -1,12 +1,25 @@
 package com.example.trashapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        val registerButton = findViewById<Button>(R.id.buttonRegister)
+        registerButton.setOnClickListener{
+            // TODO - add the user to DB
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        val registerToLoginButton = findViewById<Button>(R.id.buttonRegisterToLogin)
+        registerToLoginButton.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
