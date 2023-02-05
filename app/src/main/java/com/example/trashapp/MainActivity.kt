722e2fd433e9070804efb75d4acce36cfe36e259
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-02-05 14:14:29 +0100

    Changing navbar depending on role (in progress)

--- app/src/main/java/com/example/trashapp/MainActivity.kt
+++ app/src/main/java/com/example/trashapp/MainActivity.kt
@@ -53,6 +53,39 @@ class MainActivity : AppCompatActivity(), SortButtonCallback {
                 R.id.nav_companies, R.id.nav_vehicles, R.id.nav_workers, R.id.nav_account
             ), drawerLayout
         )
+
+        val role = "admin"
+        val menu = navView.menu
+        when (role){
+            "admin" -> {
+                menu.findItem(R.id.nav_map).isVisible = true
+                menu.findItem(R.id.nav_reports).isVisible = true
+                menu.findItem(R.id.nav_reports).title = "All reports"
+                menu.findItem(R.id.nav_groups).isVisible = true
+                menu.findItem(R.id.nav_reports).title = "All reports"
+                menu.findItem(R.id.nav_collecting_points).isVisible = true
+                menu.findItem(R.id.nav_users).isVisible = true
+                menu.findItem(R.id.nav_companies).isVisible = true
+                menu.findItem(R.id.nav_vehicles).isVisible = true
+                menu.findItem(R.id.nav_workers).isVisible = true
+                menu.findItem(R.id.nav_account).isVisible = true
+            }
+            "user" -> {
+                menu.findItem(R.id.nav_map).isVisible = true
+                menu.findItem(R.id.nav_reports).isVisible = true
+                menu.findItem(R.id.nav_reports).title = "Your reports"
+                menu.findItem(R.id.nav_groups).isVisible = true
+                menu.findItem(R.id.nav_groups).title = "Your groups"
+                menu.findItem(R.id.nav_collecting_points).isVisible = true
+                menu.findItem(R.id.nav_users).isVisible = false
+                menu.findItem(R.id.nav_companies).isVisible = false
+                menu.findItem(R.id.nav_vehicles).isVisible = false
+                menu.findItem(R.id.nav_workers).isVisible = false
+                menu.findItem(R.id.nav_account).isVisible = true
+            }
+        }
+
+
         val headerView: View = binding.navView.getHeaderView(0)
         val navUsername = headerView.findViewById<View>(R.id.textViewLoginNav) as TextView
         navUsername.text = getSharedPreferences("credentials", Context.MODE_PRIVATE).getString("login", "")
