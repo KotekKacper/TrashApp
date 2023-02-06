f5c851ae98a99cef8767aa3c299254281b735369
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-02-06 20:49:26 +0100

    Fixes to login

--- app/src/main/java/com/example/trashapp/DBUtils.kt
+++ app/src/main/java/com/example/trashapp/DBUtils.kt
@@ -1210,6 +1210,8 @@ object DBUtils {
                         val editor = preferences.edit()
 
                         editor.putString("role", json.toString().trimEnd(','))
+                        editor.putString("login", username)
+                        editor.putString("password", Encryption.decrypt(password))
                         editor.apply()
                         context.startActivity(Intent(context, MainActivity::class.java))
                         (context as Activity).finish()
