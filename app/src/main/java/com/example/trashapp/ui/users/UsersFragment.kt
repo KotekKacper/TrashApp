3e2a6b897602b73b42e643b7048ad8a87f7c094b
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-02-06 12:56:41 +0100

    Sort menu in users + email in users items

--- app/src/main/java/com/example/trashapp/ui/users/UsersFragment.kt
+++ app/src/main/java/com/example/trashapp/ui/users/UsersFragment.kt
@@ -1,5 +1,6 @@
 package com.example.trashapp.ui.users
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,13 +13,15 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trashapp.DBUtils
+import com.example.trashapp.MainActivity
 import com.example.trashapp.R
+import com.example.trashapp.SortButtonCallback
 import com.example.trashapp.adapters.CollectingPointItemAdapter
 import com.example.trashapp.adapters.UserItemAdapter
 import com.example.trashapp.databinding.FragmentUsersBinding
 import com.example.trashapp.ui.collectingpoints.AddPointActivity
 
-class UsersFragment : Fragment() {
+class UsersFragment : Fragment(), SortButtonCallback {
 
     private var _binding: FragmentUsersBinding? = null
 
@@ -36,6 +39,11 @@ class UsersFragment : Fragment() {
 
         _binding = FragmentUsersBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        (activity as MainActivity).let {
+            it.sortButton.visibility = View.VISIBLE
+            it.sortButton.setOnClickListener { onSortButtonClicked() }
+        }
 
         val addButton = binding.root.findViewById<Button>(R.id.buttonUsersAdd)
         addButton.setOnClickListener {
@@ -62,5 +70,25 @@ class UsersFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onSortButtonClicked() {
+        val sortOptions = arrayOf("Login (A-Z)","Login (Z-A)",
+            "Roles (A-Z)","Roles (Z-A)",
+            "Email (A-Z)","Email (Z-A)")
+        val builder = AlertDialog.Builder(context)
+        val adapter = DBUtils.usersAdapter
+        builder.setTitle("Sort by")
+            .setItems(sortOptions) { _, which ->
+                when (which) {
+                    0 -> adapter.sortByLoginAscending(context!!)
+                    1 -> adapter.sortByLoginDescending(context!!)
+                    2 -> adapter.sortByRolesAscending(context!!)
+                    3 -> adapter.sortByRolesDescending(context!!)
+                    4 -> adapter.sortByEmailAscending(context!!)
+                    5 -> adapter.sortByEmailDescending(context!!)
+                }
+            }
+            .show()
     }
 }
